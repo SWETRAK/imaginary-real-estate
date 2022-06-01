a76917d6e4f6f3c1d83edbe654363b2083f55b76
Kamil Pietrak <kamilpietrak123@gmail.com>
2022-06-01 20:52:08 +0200

    Added favourites and cleaned up the code

--- src/main/resources/static/public/js/favourite.js
+++ src/main/resources/static/public/js/favourite.js
@@ -32,16 +32,18 @@ const removeLike = (offer) => {
     let newLiked = JSON.stringify(likedJSON);
     localStorage.setItem(likedKey, newLiked);
     debugLocalStorage();
+
+    loadContent();
+
 }
 
 const loadContent = () => {
     const liked = localStorage.getItem(likedKey);
     if(liked === null) {
-        console.log("brak zgloszeń");
         return;
     }
 
-    console.log("sa zgloszenia");
+    let myDiv = document.getElementById("dynamic-content");
 
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -53,37 +55,50 @@ const loadContent = () => {
         redirect: 'follow'
     };
 
+    myDiv.innerHTML = `<div id="progress">
+                            <div class="my-margin">
+                                <progress class="progress is-success is-small" max="100">20%</progress>
+                            </div>
+                        </div>`;
+
+
     fetch("https://irea-app.herokuapp.com/api/v1/get/liked", requestOptions)
         .then(response => response.json())
         .then(result => {
-            let myDiv = document.getElementById("dynamic-content");
             myDiv.innerHTML = "";
-            console.log(result);
-            result.forEach((offer) => {
-                myDiv.innerHTML += "" +
-                    "    <div class=\"card my-margin\">\n" +
-                    "        <div class=\"my-card-content\">\n" +
-                    "            <div class=\"my-media\">\n" +
-                    "                <figure class=\"my-400x300\">\n" +
-                    "                    <img src=\"https://spring-irea.s3.eu-central-1.amazonaws.com/"+ offer.frontImage.imageFileName +"\"\n" +
-                    "                         alt=\"Placeholder image\">\n" +
-                    "                </figure>\n" +
-                    "            </div>\n" +
-                    "            <div class=\"my-media-content\">\n" +
-                    "                <p class=\"title is-4\">" + offer.title + "</p>\n" +
-                    "                <p class=\"subtitle is-6\">" + offer.address + "</p>\n" +
-                    "                <p class=\"is-4\"> Area: <strong>" + offer.area + "m2</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i\n" +
-                    "                    class=\"fa-solid fa-bed\"></i> <strong>" + offer.bedrooms + "</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i\n" +
-                    "                    class=\"fa-solid fa-bath\"></i> <strong>" + offer.bathrooms + "</strong></p>\n" +
-                    "                <div class=\"content\">\n" + offer.description + "\n" +
-                    "                </div>\n" +
-                    "                <a href=\"/details/" + offer.id + "\" class=\"button is-info\">Details</a>\n" +
-                    "                <br>\n" +
-                    "                    <button class=\"button is-danger\" onClick=\"removeLike(" + offer.id + ")\">Unlike</button>\n" +
-                    "            </div>\n" +
-                    "        </div>\n" +
-                    "    </div>"
-            })
+            if(result.length === 0) {
+                myDiv.innerHTML = `<div class="my-margin">
+                                        <div class="notification is-danger">
+                                            <h2 class="subtitle has-text-white">Ups!!! You haven't got liked posts. Go to <a href="/offers">offers page</a> and make some love.</h2>
+                                        </div>
+                                    </div>`;
+            } else {
+                result.forEach((offer) => {
+                    myDiv.innerHTML += "" +
+                        "    <div class=\"card my-margin\">\n" +
+                        "        <div class=\"my-card-content\">\n" +
+                        "            <div class=\"my-media\">\n" +
+                        "                <figure class=\"my-400x300\">\n" +
+                        "                    <img src=\"https://spring-irea.s3.eu-central-1.amazonaws.com/"+ offer.frontImage.imageFileName +"\"\n" +
+                        "                         alt=\"Placeholder image\">\n" +
+                        "                </figure>\n" +
+                        "            </div>\n" +
+                        "            <div class=\"my-media-content\">\n" +
+                        "                <p class=\"title is-4\">" + offer.title + "</p>\n" +
+                        "                <p class=\"subtitle is-6\">" + offer.address + "</p>\n" +
+                        "                <p class=\"is-4\"> Area: <strong>" + offer.area + "m2</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i\n" +
+                        "                    class=\"fa-solid fa-bed\"></i> <strong>" + offer.bedrooms + "</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i\n" +
+                        "                    class=\"fa-solid fa-bath\"></i> <strong>" + offer.bathrooms + "</strong></p>\n" +
+                        "                <div class=\"content\">\n" + offer.description + "\n" +
+                        "                </div>\n" +
+                        "                <a href=\"/details/" + offer.id + "\" class=\"button is-info\">Details</a>\n" +
+                        "                <br>\n" +
+                        "                    <button class=\"button is-danger\" onClick=\"removeLike(" + offer.id + ")\">Unlike</button>\n" +
+                        "            </div>\n" +
+                        "        </div>\n" +
+                        "    </div>"
+                });
+            }
         })
         .catch(error => console.log('error', error));
 }
